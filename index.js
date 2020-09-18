f473037b7f07c0f1f592974d71ae5665ad9358fb
EyPelz <eypelzig@gmail.com>
2020-09-18 10:32:44 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.TOKEN;
+
+// Created instance of TelegramBot
+const bot = new TelegramBot(token, {
+  polling: true,
+});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+bot.onText(/\/scoreboard/, (msg, match) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage();
+});
