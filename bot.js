64703d333015f11c970689e4e50dc5e1127b6d36
EyPelz <eypelzig@gmail.com>
2020-10-20 00:10:16 +0300

    changes to bot token

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 const token = process.env.TOKEN;
+if (process.env.MODE === "test") token = process.env.TOKEN_TEST;
 
 // Created instance of TelegramBot
 const bot = new TelegramBot(token, {
