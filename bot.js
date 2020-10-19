b93016d9087956393f18a0e2e6994e49501530d1
EyPelz <eypelzig@gmail.com>
2020-10-20 00:10:48 +0300

    let instead of const

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const TelegramBot = require("node-telegram-bot-api");
-const token = process.env.TOKEN;
+let token = process.env.TOKEN;
 if (process.env.MODE === "test") token = process.env.TOKEN_TEST;
 
 // Created instance of TelegramBot
