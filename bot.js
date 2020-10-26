9d75c24b95eeab3c788969dd37e00241a0d0e240
EyPelz <eypelzig@gmail.com>
2020-10-27 00:13:55 +0200

    more changes to prod start

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const TelegramBot = require("node-telegram-bot-api");
 let token = process.env.TOKEN;
 const port = process.env.PORT || 8443;
 const host = process.env.HOST;
+const heroku = process.env.URL;
 let bot;
 
 // test variables
@@ -14,7 +15,10 @@ if (process.env.MODE === "test") {
   });
 } else {
   // create prod instance
+  console.log("port: ", port);
+  console.log("host: ", host);
   bot = new TelegramBot(token, { webHook: { port: port, host: host } });
+  bot.setWebHook(`${heroku}`);
 }
 
 module.exports = bot;
